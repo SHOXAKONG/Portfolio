f7aedb2de9b131b58f87b4bc28d2d7133ee377c0
SHOXAKONG <bekmurodovshohruh0224@egmail.com>
2025-04-16 11:49:11 +0500

    Fourth Commit

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User, Role
+from .models import User, Role, Project, ProjectUser, ProjectContributor, CategoryProject, Category, Portfolio, Files, Feedback, Contact
 
 
 @admin.register(User)
@@ -25,4 +25,42 @@ class CustomUserAdmin(UserAdmin):
         ),
     )
 
-admin.site.register(Role)
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProjectUser)
+class ProjectUserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProjectContributor)
+class ProjectContributorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(CategoryProject)
+class CategoryProjectAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Files)
+class FilesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
