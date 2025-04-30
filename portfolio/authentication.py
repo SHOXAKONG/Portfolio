f702c3f71fa1470d577a6ee980284ad0456eab8f
SHOXAKONG <bekmurodovshohruh0224@egmail.com>
2025-04-30 21:19:53 +0500

    Initial Commit

--- portfolio/authentication.py
+++ portfolio/authentication.py
@@ -1,5 +1,5 @@
 from rest_framework.authentication import SessionAuthentication
-
+from whitenoise
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
     def enforce_csrf(self, request):
